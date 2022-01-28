96a06da21f388f8b4a732350c64fbef00ca348e4
emilie <emiliebs@uia.no>
2022-01-28 14:21:24 +0100

    thirdcommit

---
+++ quote.go
@@ -0,0 +1,10 @@
+package main
+
+import(
+	"fmt"
+	"github.com/emiliebstrandi/rsc/source"
+)
+
+func main(){
+	fmt.Println(src.Quote())
+}
---
+++ source/src.go
@@ -0,0 +1,9 @@
+package src
+
+import(
+	"rsc.io/quote"
+)
+
+func Quote() string{
+	return quote.Hello()
+}
